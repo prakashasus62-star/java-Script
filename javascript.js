4078022f6cde02bb604cc1b435265d59f865e51d
prakash p <prakashasus62@gmail.com>
2026-02-03 14:16:32 +0530

    js
    javaScipt practice question

---
+++ javascript.js
@@ -0,0 +1,237 @@
+// var a = 10
+// console.log(a);
+// // var a = 9
+// console.log(a);
+// // var include re-declaration and intial
+// let number1 = 10
+// console.log(number1)
+// // reclaration not work and intial work
+// console.log(a);
+// a = 15
+// console.log('a', a)
+// // conststant not change value include re declaration anf initial
+// const pk = 10
+// console.log(pk)
+// // arirhimitic operator+-*%
+// // comparison operator>,<,==,<=,>=,
+// let num = 10
+// let cum = 9
+// let result = num+cum
+// console.log(result)
+
+// // if statement
+// // if(condition){
+// let student = 'pk'
+// if(student == 'pk') {
+//     console.log(student);
+// }
+// else{
+//     console.log('not p')
+// }
+
+
+
+// let age=prompt('Enter your age')
+// if(age >=18){
+//     console.log('vote podalam')
+// }
+// else{
+//     console.log('mudiyathu age is low')
+// }
+// if else staement 
+// let cricket=prompt('enter favourite: ')
+// if(cricket=='dhoni fan'){
+//     console.log('finisher')
+// }
+// else if(cricket=='vk fan'){
+//     console.log('classic')
+// }
+// else if(cricket=='pandya fan'){
+//     console.log('attitute')
+// }
+// else if(cricket=='vaibav'){
+//     console.log('little king')
+// }
+// else if(cricket=='bumrah fan'){
+//     console.log('attack bowling')
+// }
+// else if(cricket=='hit man'){
+//     console.log('hitter')
+// }
+// else{
+//     console.log('iam kabaddi player')
+// }
+
+
+// let player='dhoni'
+// player=='dhoni'?console.log('finisher'):console.log('wicketkeeper')
+// let i=0;
+// while(i<100){
+//     console.log('pk',i)
+//     i++
+// }
+    
+// let a=PublicKeyCredential
+// console.log(typeof a)
+// let b=praksh
+// console.log(typeof b)
+// let c=10
+// console.log(typeof c)
+
+// string
+// let ename='prakash'
+// console.lcog(ename.toUpperCase())
+
+// let sentence='                 '
+// console.log(space.length)
+//  task/////////////
+//  let sname='prakash'
+//  let vowel='aeiouAEIOU'
+//  for(let i=0; i<sname.length; i++){
+//     if(vowel.includes(ename(i)))
+//         console.log(i)
+// }
+
+ 
+//  let friends= ['naveen','kavin','sacin']
+
+// // for (let i = 0; i < name.length; i++) {
+// //     console.log(name[i]);
+// // }
+// for (let index in friends){
+//     console.log(index)
+// } 
+// add the value simple method
+// let add =[2,5,8,4,1,65,98];
+// cn=0
+// for(let value of add){
+//     cn=cn+value;
+
+// }
+// console.log(cn)
+// odd and even
+// let i=[2,51,8,4,100,98];
+// let result=0;
+// for (let value of i){
+//     if(value%2== 0){
+//         console.log(value,'even')
+//     }
+//     else{
+//         console.log(value,'odd')
+
+//     }
+// }
+// object 
+
+// let dhoni={
+//     sname:['pk','pk'],
+//     age:19,
+//     game:'cricket',
+//     skills:['silent'],
+//     phone:9095986398,
+// }
+// console.log(dhoni.skills)
+// console.log(Object.keys(dhoni))
+// console.log(Object.values(dhoni))
+// console.log(Object.entries(dhoni))
+
+// dhoni.sname='prakash'
+// dhoni.age=20
+// console.log(dhoni)
+
+// for (let i in dhoni){
+//     console.log(i)
+// }
+
+// function
+
+
+// function sayhello() {
+//     return'hello'
+// }
+// console.log(sayhello())
+
+
+// function Dhoni() {
+//     return'THALA DA'
+// }
+// console.log(Dhoni())
+// console.log(Dhoni())
+
+
+// function PK() {
+//     return'NAVEEN THA eppavumea kinguuuuu'
+// }
+// console.log(PK())
+// console.log(PK())
+
+// function PK() {
+//     return 4494885884859849848358938834297+9589827895575285298226695
+// }
+// console.log(PK())
+// console.log(PK())
+
+// function additon(a,b) {
+//     return a-b
+    
+// }
+// console.log(additon(10,10))
+
+// // multification
+
+// function additon(a,b) {
+//     return a+b
+    
+// }
+// console.log(additon(10,10))
+
+// anonyms function
+
+// (function() {
+//     console.log('hi')
+    
+// }())
+// (function(pk) {
+//     console.log(pk)
+    
+// }("pk da"))
+    
+// let fun=function(data) {
+//     return(data)
+    
+// }
+// console.log(fun('prashant'))
+// arrow function
+// (parameters)=>{
+// code}
+
+// let fun=(a,b)=>{
+//     return a*b
+// }
+// console.log(fun(23,76)
+// function additon(a,b) {
+//     return a+b
+    
+// }
+
+
+// function calculator(a,b,fun) {
+//     return fun(a,b)
+    
+// }
+// console.log(calculator(9,0,additon))
+
+// for (let i = 0; i>10; i++) 
+//     console.log(i)
+
+let num=8
+
+for(let i=1; i<num; i++ ){
+    if(num%i==0){
+        console.log('not a prime')
+    }
+    else{
+        console.log('prime')
+    }
+}
+
