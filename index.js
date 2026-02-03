1571c3c7a35854c253f07e0d5e1a955be2e3df80
prakash p <prakashasus62@gmail.com>
2026-02-03 14:28:46 +0530

    js
    java practice question

---
+++ index.js
@@ -0,0 +1,176 @@
+// for (let i = 0; i<10; i++)
+//     console.log(i)
+
+
+// for (let i = 0; i<=20; i++){
+//     if (i%2==0) {
+//         console.log(i)
+        
+//     }
+// }
+
+// for (let i = 10; i>=1; i--){
+//     console.log(i)
+// }
+
+// for (let i = 0; i<=15; i++){
+//     if (i%2 !==0) {
+//         console.log(i)
+        
+//     }
+// }
+//  for (let i = 0; i>=5; i++){
+//     console.log(i**2)
+
+//  }
+
+
+
+// let sum = 0;
+// for (let i = 1; i <= 100; i++) {
+//   sum += i;
+// }
+// console.log("Sum from 1 to 100 is:", sum);
+
+// for (let i = 1; i <= 10; i++){
+//   console.log(i*7)
+// }
+
+
+
+// for (let i = 0; i<=50; i++){
+//     if (i%5==0) {
+//         console.log(i)
+        
+//     }
+// }
+
+// for (let i = 0; i<=30; i++){
+//     console.log(i*3)
+//   }
+
+// 
+
+// let text = "JavaScript";
+// for (let i = 0; i < text.length; i++) {
+//   console.log(text[i]);
+// }
+
+
+// let fruits = ["apple", "banana", "mango"];
+// for (let i = 0; i < fruits.length; i++){
+//   console.log(fruits[i]);
+// }
+
+// let sum=0
+// for (let i = 10; i <= 40; i++){
+//   sum+=i
+//   console.log("Sum from 10 to 40 is:", sum)
+// }
+// let pk='hello'
+// let rev=pk.split("").reverse().join('')
+// console.log(rev)
+
+// let colors = ["red", "green", "blue", "yellow", "pink"];
+// let sum=1
+// for (let i = 0; i < colors.length; i++){
+//   sum-=i
+//   console.log(i)
+
+// }
+ let a=10
+// let b=8
+// if(a>b){
+//     console.log('larger number is:'+a)
+// }
+// else{
+//     console.log('larger number is:'+b)
+
+// }
+
+//
+// let age =18
+// if(age==18){
+//     console.log('eligible')
+// }
+// else{
+//     console.log('not eligible')
+// }
+
+// if (num % 5 === 0 && num % 11 === 0) {
+// console.log("Divisible by 5 and 11");
+// } else {
+// console.log("Not divisible by 5 and 11");
+// }
+
+
+// Given a score, assign grades as follows:
+// •
+// A: 90 and above
+// •
+// B: 80–89
+// •
+// C: 70–79
+// •
+// D: 60–69
+// •
+// F: Below 60
+// Use else if ladder
+
+
+// let score=1
+// if(score>90){
+//     console.log('90 and above')
+// }
+// if(score>89){
+//     console.log('80 to 89')
+// }
+// if(score>80){
+//     console.log('70 to 79')
+// }
+// if(score>70){
+//     console.log('60 to 69')
+// }
+// else{
+//     console.log('below 60')
+// }
+// let sname = 'p'; // You can change 'p' to any string you'd like to test
+// let vowel = 'aeiouAEIOU';
+
+// for (let i = 0; i < sname.length; i++) {
+//     if (vowel.includes(sname[i])) {
+//         console.log(i); // Prints the index of the vowel
+//     } else {
+//         console.log('illa'); // Prints 'illa' if it's not a vowel
+//     }
+// }
+
+// let p=150
+// if(p>100){
+//     console.log('ama')
+// }
+// else{
+//     xonsole.log('illa')
+// }
+// let a=4
+// let b=8
+// let c=9
+// if(a%3==0){
+//     console.log('divisible by 3')
+// }
+// if(b%3==0){
+//     console.log('divisible by 3')
+// }
+// if(c%3==0){
+//     console.log(' 9 divisible by 3')
+// }
+// else{
+//     console.log('illa')
+// }
+let a=4
+if(a%3==0){
+    console.log('divisible by 3')
+}
+else{
+    console.log('illa')
+}
