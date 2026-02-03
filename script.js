1ed4e865ddf32cf92460d1979dea41de5d55aee4
prakash p <prakashasus62@gmail.com>
2026-02-03 14:25:43 +0530

    js practice code
    js

---
+++ script.js
@@ -0,0 +1,140 @@
+/////////////
+//  let sname='prakash'
+//  let vowel='aeiouAEIOU'
+//  for(let i=0; i<sname.length; i++){
+//     if(vowel.includes(ename(i)))
+//         console.log(i)
+// }
+
+ 
+//  let friends= ['naveen','kavin','sacin']
+
+// // for (let i = 0; i < name.length; i++) {
+// //     console.log(name[i]);
+// // }
+// for (let index in friends){
+//     console.log(index)
+// } 
+// add the value simple method
+// let add =[2,5,8,4,1,65,98];
+// cn=0
+// for(let value of add){
+//     cn=cn+value;
+
+// }
+// console.log(cn)
+// odd and even
+// let i=[2,51,8,4,100,98];
+// let result=0;
+// for (let value of i){
+//     if(value%2== 0){
+//         console.log(value,'even')
+//     }
+//     else{
+//         console.log(value,'odd')
+
+//     }
+// }
+// object 
+
+// let dhoni={
+//     sname:['pk','pk'],
+//     age:19,
+//     game:'cricket',
+//     skills:['silent'],
+//     phone:9095986398,
+// }
+// console.log(dhoni.skills)
+// console.log(Object.keys(dhoni))
+// console.log(Object.values(dhoni))
+// console.log(Object.entries(dhoni))
+
+// dhoni.sname='prakash'
+// dhoni.age=20
+// console.log(dhoni)
+
+// for (let i in dhoni){
+//     console.log(i)
+// }
+
+// function
+
+
+// function sayhello() {
+//     return'hello'
+// }
+// console.log(sayhello())
+
+
+// function Dhoni() {
+//     return'THALA DA'
+// }
+// console.log(Dhoni())
+// console.log(Dhoni())
+
+
+// function PK() {
+//     return'NAVEEN THA eppavumea kinguuuuu'
+// }
+// console.log(PK())
+// console.log(PK())
+
+// function PK() {
+//     return 4494885884859849848358938834297+9589827895575285298226695
+// }
+// console.log(PK())
+// console.log(PK())
+
+// function additon(a,b) {
+//     return a-b
+    
+// }
+// console.log(additon(10,10))
+
+// // multification
+
+// function additon(a,b) {
+//     return a+b
+    
+// }
+// console.log(additon(10,10))
+
+// anonyms function
+
+// (function() {
+//     console.log('hi')
+    
+// }())
+// (function(pk) {
+//     console.log(pk)
+    
+// }("pk da"))
+    
+// let fun=function(data) {
+//     return(data)
+    
+// }
+// console.log(fun('prashant'))
+// arrow function
+// (parameters)=>{
+// code}
+
+// let fun=(a,b)=>{
+//     return a*b
+// }
+// console.log(fun(23,76)
+// function additon(a,b) {
+//     return a+b
+    
+// }
+
+
+// function calculator(a,b,fun) {
+//     return fun(a,b)
+    
+// }
+// console.log(calculator(9,0,additon))
+
+
+for (let i = 0; i>10; i++) 
+    console.log(i)
